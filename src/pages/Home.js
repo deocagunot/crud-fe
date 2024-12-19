4ed0eeec9999271dfc642ee8cbddfb217267a9f3
deocagunot <deo.cagunot@gmail.com>
2024-12-19 20:07:20 +0800

    navlink route changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Home = () => (
   <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
@@ -12,9 +12,9 @@ const Home = () => (
       </p>
 
       <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-        <Link
+        <NavLink
           to="posts"
-          className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+          className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
         >
           Check Post
           <svg
@@ -26,13 +26,13 @@ const Home = () => (
           >
             <path stroke="currentColor" d="M1 5h12m0 0L9 1m4 4L9 9" />
           </svg>
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           to="about"
           className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
         >
           About Us
-        </Link>
+        </NavLink>
       </div>
     </div>
   </section>
